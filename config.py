6f128f0008ec88a92987e076774e7749fe4437f6
Alberto Burgos <aburgos@cimne.upc.edu>
2020-07-30 12:06:34 +0200

    Add more configs

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 DATA_PATH = "C:/Users/alberto/Documents/datasets/global-wheat-detection"
 BATCH_SIZE = 4
-NUM_WORKERS = 4
+NUM_CLASSES = 2 # 1 class (wheat) + background
+NUM_WORKERS = 0
 EPOCHS = 50
 DEVICE = "cuda"
+LR = 0.003
