f4b8bc382b4b9fa73d4f4ad917903942beba17c1
Alberto Burgos <aburgos@cimne.upc.edu>
2020-07-30 15:37:29 +0200

    Add different BS and debug

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 DATA_PATH = "C:/Users/alberto/Documents/datasets/global-wheat-detection"
-BATCH_SIZE = 4
+TRAIN_BATCH_SIZE = 4
+VALID_BATCH_SIZE = 2
 NUM_CLASSES = 2 # 1 class (wheat) + background
 NUM_WORKERS = 0
-EPOCHS = 50
+EPOCHS = 10
 DEVICE = "cuda"
-LR = 0.003
+LR = 0.001
+DEBUG = True
