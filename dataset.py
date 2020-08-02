0630271fafd9bfea2d2a1e3bcf68f1d1652a913d
Alberto Burgos <aburgos@cimne.upc.edu>
2020-08-02 15:53:28 +0200

    Add more augs

--- dataset.py
+++ dataset.py
@@ -69,7 +69,8 @@ class WheatDataset(Dataset):
                     'labels': labels
                 })
                 image = sample['image']
-                target['boxes'] = torch.stack(tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1, 0).float()
+                if len(sample['bboxes']) > 0:
+                    target['boxes'] = torch.stack(tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1, 0).float()
 
             return image, target
 
@@ -136,13 +137,25 @@ def get_test_data_loader():
 
 def get_train_transforms():
     return A.Compose([
-        A.Flip(p=0.5),
-        ToTensorV2(p=1.0)
+        A.RandomSizedCrop(min_max_height=(800, 800), height=1024, width=1024, p=0.5),
+        A.OneOf([
+            A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit= 0.2, 
+                                 val_shift_limit=0.2, p=0.9),
+            A.RandomBrightnessContrast(brightness_limit=0.2, 
+                                       contrast_limit=0.2, p=0.9),
+            ], p=0.9),
+        A.ToGray(p=0.01),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.Resize(height=512, width=512, p=1.0),
+        A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
+        ToTensorV2(p=1.0),
     ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
 
 
 def get_valid_transforms():
     return A.Compose([
+        A.Resize(height=512, width=512, p=1.0),
         ToTensorV2(p=1.0)
     ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
 
